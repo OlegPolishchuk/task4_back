08d13f1542953120c3941055e1a5a4bd3c33be69
OlegPolishchuk <oleg.makkorm@gmail.com>
2022-11-20 15:15:58 +0300

    cors options

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ const DB_PASSWORD = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 
 
-app.use(cors({
-  origin: "http://localhost:3000",
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
